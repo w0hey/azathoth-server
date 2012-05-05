ce71377f0cc3c1165566a9c0e1e488b75cc694ee
Steve McGrath <smcgrath23@gmail.com>
2012-05-05 02:03:26 -0500

    I think this might be better

--- services/robotservice.py
+++ services/robotservice.py
@@ -12,13 +12,13 @@ class RobotService(service.Service):
 
     def startService(self):
         log.msg("robotservice starting")
+        service.Service.startService(self)
         self.driveservice = self.top_service.getServiceNamed('driveservice')
         self.ioservice = self.top_service.getServiceNamed('ioservice')
         self.controlservice = self.top_service.getServiceNamed('controlservice')
         self.shellservice = self.top_service.getServiceNamed('shellservice')
         self.drive = Drive(self)
         self.lcd = Lcd(self)
-        service.Service.startService(self)
 
     def stopService(self):
         log.msg("robotservice stopping")
