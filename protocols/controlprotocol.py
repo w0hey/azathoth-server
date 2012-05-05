e1f6cf1dec9620fb5aa19dacc0d5e20a240581d8
Steve McGrath <smcgrath23@gmail.com>
2012-05-04 21:35:53 -0500

    finish send_calibration()
    it's not that useful unless it actually sends something

--- protocols/controlprotocol.py
+++ protocols/controlprotocol.py
@@ -38,4 +38,6 @@ class ControlProtocol(NetstringReceiver):
         cur_y = d['current_y']
         eeprom_x = d['eeprom_x']
         eeprom_y = d['eeprom_y']
+        data = chr(cur_x) + chr(cur_y) + chr(eeprom_x) + chr(eeprom_y)
+        self.sendString(data)
 
