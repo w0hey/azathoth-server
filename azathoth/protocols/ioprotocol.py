581bc6d808e9663b327033d630b93d5d6f7daf92
Steve McGrath <smcgrath23@gmail.com>
2012-05-15 21:12:03 -0500

    Adapt IoProtocol to callback structure like DriveProtocol

--- azathoth/protocols/ioprotocol.py
+++ azathoth/protocols/ioprotocol.py
@@ -1,12 +1,27 @@
+from twisted.python import log
+
 from azathoth.protocols.linkprotocol import LinkProtocol
 
 class IoProtocol(LinkProtocol):
     def __init__(self, service):
         self.service = service
+        self.callbacks = {}
         LinkProtocol.__init__(self)
 
+    def register_callback(self, cmd, callback):
+        self.callbacks[cmd] = callback
+
+    def unregister_callback(self, packet):
+        del self.callbacks[cmd]
+
     def handle_packet(self, packet):
-        pass
+        data = map(ord, packet)
+        log.msg(system='IoProtocol', format="Got packet: %(data)s", data=map(hex,data))
+        cmd = data[0]
+        if cmd in self.callbacks:
+            self.callbacks[cmd](data[1:])
+        else:
+            log.msg(system='IoProtocol', format="No callback for command: %(cmd)s", cmd=cmd)
 
     def handle_badframe(self, data):
         log.err(system="IoProtocol", format="bad frame received: %(data)s", data=data)
