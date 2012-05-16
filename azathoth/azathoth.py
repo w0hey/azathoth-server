350f2fc2a6d33b85b627285d42cfdc59d98d97da
Steve McGrath <smcgrath23@gmail.com>
2012-05-15 21:23:04 -0500

    Use the command line options provided

--- azathoth/azathoth.py
+++ azathoth/azathoth.py
@@ -8,25 +8,20 @@ from factories.shellfactory import ShellFactory
 from factories.controlfactory import ControlFactory
 
 
-telnet_port = 2023
-control_port = 2024
-driveport = '/dev/arduino_A9007MiE'
-ioport = '/dev/arduino_A900ae1d'
-
 def makeService(options):
     top_service = service.MultiService()
 
     robot_service = RobotService(top_service)
     robot_service.setServiceParent(top_service)
 
-    drive_service = DriveService(driveport, 115200)
+    drive_service = DriveService(options['drive'], 115200)
     drive_service.setServiceParent(robot_service)
 
-    io_service = IoService(ioport, 115200)
+    io_service = IoService(options['io'], 115200)
     io_service.setServiceParent(robot_service)
 
     shell_factory = ShellFactory(top_service)
-    shell_service = internet.TCPServer(telnet_port, shell_factory)
+    shell_service = internet.TCPServer(int(options['telnetport']), shell_factory)
     shell_service.setName('shellservice')
     shell_service.setServiceParent(top_service)
 
