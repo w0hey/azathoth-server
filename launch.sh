cebd467929df0df2f83eef730a12d5394d8c79cc
Steve McGrath <smcgrath23@gmail.com>
2012-05-24 20:44:33 -0500

    simple launch script to make testing easier

---
+++ launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while getopts "t" opt; do
+	case $opt in
+		t) testmode=true
+	esac
+done
+
+if [[ $testmode = true ]]; then
+	socat PTY,link=$HOME/COM1 PTY,link=$HOME/COM2 &
+	socat PTY,link=$HOME/COM3 PTY,link=$HOME/COM4 &
+	twistd -n azathoth --io=$HOME/COM1 --drive=$HOME/COM3
+else
+	twistd -n azathoth
+fi
