d218c477bb264cf991c9e369b364c49b36ff622a
Steve McGrath <smcgrath23@gmail.com>
2013-06-13 20:40:35 -0500

    launch script should clean up after itself
    after server shuts down, delete the fake serial devices created with SOCAT
    for testing

--- launch.sh
+++ launch.sh
@@ -10,6 +10,7 @@ if [[ $testmode = true ]]; then
 	socat PTY,link=$HOME/COM1 PTY,link=$HOME/COM2 &
 	socat PTY,link=$HOME/COM3 PTY,link=$HOME/COM4 &
 	twistd -n azathoth --io=$HOME/COM1 --drive=$HOME/COM3
+	rm $HOME/COM*
 else
 	twistd -n azathoth
 fi
